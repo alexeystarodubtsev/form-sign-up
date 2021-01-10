2ff1a22068b25f6943c939d56ac3ded6a729d6cc
AlexStarodubtsev <starodub97@yandex.ru>
2021-01-10 18:41:51 +0300

    added icons

---
+++ src/stories/icon.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { LockIcon, LetterIcon, ArrowIcon } from "../components/icons";
+
+export default {
+    title: "icons",
+}
+
+export const Lock_Icon = () => (
+    <LockIcon/>
+)
+export const Letter_Icon = () => (
+    <LetterIcon/>
+)
+export const Arrow_Icon = () => (
+    <ArrowIcon/>
+)
